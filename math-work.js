67c26853a889a1c0289b9f28156737f9724d08e3
SineMag <snenhlanhlamagubane99@gmail.com>
2025-07-31 14:57:26 +0200

    JavaScript's first mathematical project, object based.

---
+++ math-work.js
@@ -0,0 +1,18 @@
+// Task 4.1 Answers 
+
+//Part 3: Coding Exercises
+
+const num1 = 4.789;
+
+num1 = Math.round(num1);
+
+const num2 = 12.345;
+
+num2 = Math.round(num2);
+
+const num3 = -8.5;
+num3 = Math.round(num3);
+
+console.log(num1);
+//console.log(num2);
+//console.log(num3);
